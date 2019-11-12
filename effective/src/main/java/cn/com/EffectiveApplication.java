c8bea8061368fbbc5251d742175102e077e2c8b0
王明超 <wangmingchao@botbrain.cn>
2019-11-12 09:07:00 +0800

    feat(all): 添加effective

---
+++ effective/src/main/java/cn/com/EffectiveApplication.java
@@ -0,0 +1,18 @@
+package cn.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Copyright：botBrain.ai
+ * Author: WangMingChao
+ * Date: 2019/11/12.
+ * Description:
+ */
+@SpringBootApplication
+public class EffectiveApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EffectiveApplication.class, args);
+    }
+
+}
