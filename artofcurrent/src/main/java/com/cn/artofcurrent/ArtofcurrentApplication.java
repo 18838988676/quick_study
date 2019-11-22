40002ddfeb5d723eaca07bb98098465b9a4744fc
王明超 <wangmingchao@botbrain.cn>
2019-11-22 10:24:56 +0800

    feat(all): 插件测试12

---
+++ artofcurrent/src/main/java/com/cn/artofcurrent/ArtofcurrentApplication.java
@@ -0,0 +1,13 @@
+package com.cn.artofcurrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtofcurrentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtofcurrentApplication.class, args);
+    }
+
+}
---
+++ artofcurrent/src/test/java/com/cn/artofcurrent/ArtofcurrentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cn.artofcurrent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtofcurrentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
