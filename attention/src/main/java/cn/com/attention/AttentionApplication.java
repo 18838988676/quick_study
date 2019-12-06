014bcbdcee6dd19843a116ada50d3c0986b7d459
王明超 <wangmingchao@botbrain.cn>
2019-12-06 23:37:09 +0800

    feat(all): add

---
+++ attention/src/main/java/cn/com/attention/AttentionApplication.java
@@ -0,0 +1,13 @@
+package cn.com.attention;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttentionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttentionApplication.class, args);
+    }
+
+}
---
+++ attention/src/test/java/cn/com/attention/AttentionApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.com.attention;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AttentionApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
