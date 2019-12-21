35fc5d24090a9db9dbab5c239b1a05485f2aa3cb
18838988676@163.com <18838988676@163.com>
2019-12-21 11:57:50 +0800

    add 编程思想

---
+++ pragromideas/src/main/java/com/PragromideasApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PragromideasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PragromideasApplication.class, args);
+    }
+
+}
